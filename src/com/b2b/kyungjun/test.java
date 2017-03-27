e74609d725f30ee9cf51b39e7d723f103f03649b
bisu77 <rudwns0615@naver.com>
2017-03-27 20:37:55 +0900

    second commit by KyungJun

--- src/com/b2b/kyungjun/test.java
+++ src/com/b2b/kyungjun/test.java
@@ -1,3 +1,8 @@
+package com.b2b.kyungjun;
+
+/**
+ * Created by KyungJun on 2017-03-27.
+ */
 public class test{
   public static void main(String[] args){
     System.out.println("Hello World! KyungJun!");
